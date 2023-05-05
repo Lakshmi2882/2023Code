06d5d04612b1bc271bbdedaed7009e352be26ce8
Lakshmi <sbharadwaj06@gmail.com>
2023-05-05 09:12:45 +0100

    adding new files

--- src/main/java/com/qa/DriverFactory/DriverFactory.java
+++ src/main/java/com/qa/DriverFactory/DriverFactory.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
@@ -11,7 +13,11 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import com.qa.UtilPackage.OptionsManager;
 
@@ -33,14 +39,29 @@ public WebDriver Init_driver(Properties prop)
 	{
 		WebDriverManager.chromedriver().setup();
 	//	driver=new ChromeDriver();
+		if(Boolean.parseBoolean(prop.getProperty("remote")))
+		{
+			init_removeDriver("chrome");
+		}
+		else
+		{
 		tlDriver.set(new ChromeDriver(option.getChromeOptions()));
+		}
 	}
 	
 		else if(browsername.equals("firefox"))
 			{
 				WebDriverManager.firefoxdriver().setup();
 				//driver=new FirefoxDriver();
-				tlDriver.set(new FirefoxDriver(option.getFirefoxOptions()));
+				if(Boolean.parseBoolean(prop.getProperty("remote")))
+				{
+					init_removeDriver("firefox");
+				}
+				else
+				{
+					tlDriver.set(new FirefoxDriver(option.getFirefoxOptions()));
+				}
+				
 			}
 		else
 		{
@@ -52,6 +73,33 @@ public WebDriver Init_driver(Properties prop)
 	getDriver().get(prop.getProperty("Url"));
 	return getDriver();
 }
+private void init_removeDriver(String browser) {
+System.out.println("Running test on remote grid server:" +browser);
+if(browser.equalsIgnoreCase("chrome"))
+{
+	DesiredCapabilities cap=new DesiredCapabilities();
+	cap.setCapability(ChromeOptions.CAPABILITY, option.getChromeOptions());
+	try {
+		tlDriver.set( new RemoteWebDriver(new URL(prop.getProperty("huburl")),option.getChromeOptions()));
+	} catch (MalformedURLException e) {
+		
+		e.printStackTrace();
+	}
+}
+	else
+	{
+		DesiredCapabilities cap=new DesiredCapabilities();
+		cap.setCapability(FirefoxOptions.FIREFOX_OPTIONS, option.getFirefoxOptions());
+		try {
+			tlDriver.set(new RemoteWebDriver(new URL(prop.getProperty("huburl")),option.getFirefoxOptions()));
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+	
+
 //This is for get driver
 public static WebDriver getDriver()
 {
